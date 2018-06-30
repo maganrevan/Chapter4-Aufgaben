23d6a974767d938e9d74b06ef3c82f24d9f3ecdf
Magnus Kruschwitz <magnus.kruschwitz@outlook.com>
2018-06-30 22:54:20 +0200

    Initial Commit

---
+++ Chapter4-Aufgaben/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  Chapter4-Aufgaben
+//
+//  Created by Magnus Kruschwitz on 30.06.18.
+//  Copyright © 2018 Magnus Kruschwitz. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
